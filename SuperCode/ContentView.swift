f6c61b11598bb8473066636af4a0abc7d6b734e6
StNiosem <bauch.aristide@gmail.com>
2024-07-17 23:45:21 +0200

    Initial Commit

---
+++ SuperCode/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SuperCode
+//
+//  Created by Aristide BAUCHART on 17/07/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SuperCode/SuperCodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SuperCodeApp.swift
+//  SuperCode
+//
+//  Created by Aristide BAUCHART on 17/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct SuperCodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
